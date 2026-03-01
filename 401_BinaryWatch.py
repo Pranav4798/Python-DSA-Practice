38abdb01ef794173d2428f646922cab9405fcecb
Pranav Bhagat <64766346+Pranav4798@users.noreply.github.com>
2026-03-01 22:54:35 +0530

    Add files via upload

---
+++ 401_BinaryWatch.py
@@ -0,0 +1,14 @@
+turnedOn = 9
+
+def BinWatch(turnedOn):
+
+    res = []
+
+    for h in range(12):
+        for m in range(60):
+            if (bin(h).count('1') + bin(m).count('1')) == turnedOn:
+                res.append(f"{h}:{m:02d}")
+
+    return res
+
+print(BinWatch(turnedOn))

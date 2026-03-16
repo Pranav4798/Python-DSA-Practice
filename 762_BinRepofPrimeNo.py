b738bdc235b8b307ba9dc90b8aea4111cf034d75
Pranav Bhagat <64766346+Pranav4798@users.noreply.github.com>
2026-03-16 19:35:15 +0530

    Quest DSA

---
+++ 762_BinRepofPrimeNo.py
@@ -0,0 +1,16 @@
+left = 6
+right = 10
+
+def BinPrime(left, right):
+    primes = {2, 3, 5, 7, 11, 13, 17, 19}
+    count = 0
+
+    for num in range(left, right + 1):
+        set_bits = bin(num).count("1")
+
+        if set_bits in primes:
+            count += 1
+
+    return count
+
+print(BinPrime(left,right))
